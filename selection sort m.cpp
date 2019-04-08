be053676b74796c9c8b8ad107c310cbf20f1148d
shivanijain1 <44234984+shivanijain1@users.noreply.github.com>
2019-04-08 14:59:56 +0530

    selection sort

---
+++ selection sort m.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int n;
+    cin>>n;
+    int a[10];
+    for (int i=0;i<n;i++)
+    {
+
+            cin>>a[i];
+
+
+    }
+for (int i=0;i<n;i++)
+{
+    for (int j=i+1;j<n;j++)
+    {
+        if (a[i]>a[j])
+        swap(a[i],a[j]);
+
+    }
+}
+for (int i=0;i<n;i++)
+{
+    cout<<a[i]<<" ";
+}
+return 0;
+}
